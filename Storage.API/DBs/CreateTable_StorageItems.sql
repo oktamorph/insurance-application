54caa2357699a217c9b4ee7c8f4eb4b4926a7caf
oktamorph <oktamorph@gmail.com>
2023-12-06 11:03:05 +0100

    update the 'CreateTable_StorageItems.sql' script

--- Storage.API/DBs/CreateTable_StorageItems.sql
+++ Storage.API/DBs/CreateTable_StorageItems.sql
@@ -4,7 +4,7 @@ go
 create table StorageItems
 (
 	ID int identity(1, 1) not null primary key,
-	InsuranceGuid uniqueidentifier not null default newid(),
+	InsuranceGuid uniqueidentifier not null default newid() unique,
 	CustomerNumber nvarchar(max) not null, --SSN
 	FirstName nvarchar(max) not null,
 	LastName nvarchar(max) not null
